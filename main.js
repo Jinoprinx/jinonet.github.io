aae2eab28ee451cd2bd459ea32373ac852b851f8
jinoprinx <90164892+Jinoprinx@users.noreply.github.com>
2023-03-11 14:22:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var string = ["Jino Prince", "Web Developer" "ML Developer", "A Python Developer"]
+
+// fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+// 	.then(response => response.json())
+// 	.then(data => {
+// 		let element =""
+// 		let dataArr = data.slice(0, 5)
+// 		for (let posts of dataArr) {
+// 			element += `
+// 				<h3>${posts.title}</h3>
+// 				<p>${posts.body}</p>
+// 				<hr />
+// 			`
+// 		}
+// 		document.getElementById('posts').innerHTML = element
+// 	})
